6318153aa04227578058df84104d9dcca8165255
Christian Rey Mora <christianmoralopez@hotmail.com>
2022-11-28 14:39:15 -0500

    Program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CoreEscuela.Entidades;
+
+namespace Estapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Howarts", 1990);
+            escuela.pais = "Inglaterra";
+            escuela.ciudad = "Londres";
+            escuela.typeSchool = TiposEscuela.Primaria;
+            Console.WriteLine("Bienvenido a " + escuela.Nombre);
+        }
+    }
+}
